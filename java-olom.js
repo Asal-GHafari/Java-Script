6dbb8684e130758b5ab1849e693f4c7efa9f4d55
Asal-GHafari <asal.ghafari2011@gmail.com>
2013-07-30 11:39:40 +0330

    i

--- java-olom.js
+++ java-olom.js
@@ -10,10 +10,34 @@ $(".le ").click(function(){
 	});	
 });
 	$(".ri ").click(function(){
-		if(c>20){c=0;
+		if(c>23){c=0;
 			$(".slider-previw2 > div > div ").each(function(){
 				$(this).animate({'top':0},'slow',"");
 			});
 		}
 	});
+	
+	
+	
+	$("#le ").click(function(){
+	c=c+1;//alert(c);
+	$("#slider-previw2 > div > div ").each(function(){
+		if(c<48){
+			$(this).animate({'top':-300*c},1000,'easeOutExpo');
+		}
+	});	
+});
+	$("#ri ").click(function(){
+		if(c>47){c=0;
+			$("#slider-previw2 > div > div ").each(function(){
+				$(this).animate({'top':0},'slow',"easeOutExpo");
+			});
+		}
+	});
+	
+	
+	
+	
+	
+	
 });
